4c0c5bb3529eee1302152ef2e3826471f55eb228
Tasneem Amreliwala <tasneemec7@gmail.com>
2019-07-03 17:25:30 -0400

    added react router challenge

---
+++ react-router-exercise/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import Greeting from './components/Greeting'
+import Goodbye from './components/Goodbye'
+import NavBar from './components/NavBar'
+import CustomGreeting from './containers/CustomGreeting'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <Router history={browserHistory}>
+        <Route path='/' component={NavBar}>
+          <Route path='/greeting' component={Greeting}/>
+          <Route path='/goodbye' component={Goodbye}/>
+          <Route path='/customgreeting' component={CustomGreeting}/>
+        </Route>
+      </Router>
+    )
+  }
+}
+
+export default App;
